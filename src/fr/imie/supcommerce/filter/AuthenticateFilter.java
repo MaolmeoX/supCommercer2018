0e384e3ba86cd50466ad7d659d6764e14fd7aa13
Maurice Arnaud <arnaud.maurice.pro@gmail.com>
2018-05-30 17:06:22 +0200

    Ajout filter et login form

---
+++ src/fr/imie/supcommerce/filter/AuthenticateFilter.java
@@ -0,0 +1,33 @@
+package fr.imie.supcommerce.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AuthenticateFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession(false);
+        if (null == session || null == session.getAttribute("username")) {
+            response.sendRedirect("/login");
+        } else if (null != session.getAttribute("username")) {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/fr/imie/supcommerce/servlet/LoginServlet.java
@@ -0,0 +1,29 @@
+package fr.imie.supcommerce.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher view = req.getRequestDispatcher("/login.html");
+        view.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+
+        HttpSession session = req.getSession();
+        if (username != null)
+            session.setAttribute("username", username);
+
+        resp.sendRedirect("/listProduct");
+    }
+}
